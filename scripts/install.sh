f5f7b6da43fd0a94f6c9c4de6e889041274610a1
Sebastian Freund <sebastian.freund@komm.one>
2024-10-09 19:47:35 +0200

    chore: better output in install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,11 +7,25 @@ REPO="abes140377/yade-generator-cli"  # Ersetze das mit deinem GitHub Repository
 # Hole die neueste Release-Version
 LATEST_RELEASE=$(curl --silent "https://api.github.com/repos/$REPO/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
+cat << "EOF"
+__  _____    ____  ______   ________    ____
+\ \/ /   |  / __ \/ ____/  / ____/ /   /  _/
+ \  / /| | / / / / __/    / /   / /    / /  
+ / / ___ |/ /_/ / /___   / /___/ /____/ /   
+/_/_/  |_/_____/_____/   \____/_____/___/   
+EOF
+
+echo "YADE CLI Installer $LATEST_RELEASE"
+echo ""
+
 # Download der neuesten Version
-echo "Downloading latest release: $LATEST_RELEASE"
-curl -L "https://github.com/$REPO/releases/download/$LATEST_RELEASE/yade" -o /usr/local/bin/yade
+echo "Downloading latest release: $LATEST_RELEASE..."
+curl -s -L "https://github.com/$REPO/releases/download/$LATEST_RELEASE/yade" -o /usr/local/bin/yade
 
 # Setze Ausführungsrechte
+echo "Change permissions..."
 chmod +x /usr/local/bin/yade
 
+echo ""
 echo "Installation complete! Run 'yade' to start."
+echo ""
